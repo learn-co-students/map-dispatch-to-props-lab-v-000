973181cd4616ef760fc15837a020d24bec05212b
David Kennell <davidkennell0@gmail.com>
2017-08-10 19:23:57 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
-
+import { bindActionCreators } from 'redux'
 export class RestaurantInput extends Component {
 
   constructor(props){
@@ -50,4 +50,8 @@ export class RestaurantInput extends Component {
   }
 };
 
-export const ConnectedRestaurantInput = connect(null, null)(RestaurantInput)
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({addRestaurant: addRestaurant}, dispatch)
+}
+
+export const ConnectedRestaurantInput = connect(null, mapDispatchToProps)(RestaurantInput)
