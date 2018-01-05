111497a29080d3e3d79f78ee84df4a4f24521218
Alexandra <github email address>
2018-01-05 12:14:54 -0800

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
-
+import { bindActionCreators } from 'redux'
 export class RestaurantInput extends Component {
 
   constructor(props){
     super(props);
 
     this.state = {
-      name: '', location: ''
+      name: '', location: '', type: ''
     };
   }
 
@@ -24,24 +24,37 @@ export class RestaurantInput extends Component {
     });
   }
 
+  handleOneTypeChange(event) {
+    this.setState({
+      type: event.target.value
+    })
+  }
+
   handleOnSubmit(event) {
     event.preventDefault();
     this.props.addRestaurant(this.state);
+
   }
 
   render() {
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnNameChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
             placeholder="restaurant name" />
         </p>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnLocationChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnTypeChange(event)}
+            placeholder="restaurant type" />
+        </p>
+        <p>
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -50,4 +63,16 @@ export class RestaurantInput extends Component {
   }
 };
 
-export const ConnectedRestaurantInput = connect(null, null)(RestaurantInput)
+const mapStateToProps = (state) => {
+  return {
+    restaurants: state.restaurants
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addRestaurant: addRestaurant
+  }, dispatch)
+}
+
+export const ConnectedRestaurantInput = connect(mapStateToProps, mapDispatchToProps)(RestaurantInput)
