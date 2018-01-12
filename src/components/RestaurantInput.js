358f3fc9ed61d1bb9cff0f0835c366ff52ebb2a8
Bruce Molina <bruce.molina.81@gmail.com>
2018-01-12 02:23:42 +0000

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux'
 
 export class RestaurantInput extends Component {
 
@@ -30,18 +31,19 @@ export class RestaurantInput extends Component {
   }
 
   render() {
+    debugger
     return(
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnNameChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnNameChange(event)}
             placeholder="restaurant name" />
         </p>
         <p>
-          <input 
-            type="text" 
-            onChange={(event) => this.handleOnLocationChange(event)} 
+          <input
+            type="text"
+            onChange={(event) => this.handleOnLocationChange(event)}
             placeholder="location" />
         </p>
         <input type="submit" />
@@ -50,4 +52,16 @@ export class RestaurantInput extends Component {
   }
 };
 
-export const ConnectedRestaurantInput = connect(null, null)(RestaurantInput)
+const mapStateToProps = () => {
+  return {
+
+    }
+  }
+
+const MapDispatchToProps = (dispatch) => {
+  return  bindActionCreators({
+    addRestaurant: addRestaurant
+  }, dispatch);
+};
+
+export const ConnectedRestaurantInput = connect(mapStateToProps, MapDispatchToProps)(RestaurantInput)
