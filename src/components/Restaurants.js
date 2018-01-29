27637930fbbb7f23fe24f8890345b8a5b9e8f0c5
Andrew <andrew.ramell@gmail.com>
2018-01-29 18:57:50 -0500

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux'
+import { addRestaurant } from '../actions/restaurants';
 
 export class Restaurants extends Component {
 
