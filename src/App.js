37360de33bdb7550199597f38e139cd6b94269d2
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-05-26 11:19:25 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { ConnectedRestaurantInput } from './components/RestaurantInput';
 import { ConnectedRestaurants } from './components/Restaurants';
+
 
 export class App extends Component {
   render() {
