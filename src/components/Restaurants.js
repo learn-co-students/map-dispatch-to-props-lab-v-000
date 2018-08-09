80eacf51372f8a52fb6370729426f680747672f1
Marissa Zhong <marissazhong@Marissas-MacBook-Pro.local>
2018-08-09 13:38:27 -0400

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 class Restaurants extends Component {
 
   render() {
-
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.restaurantName}</li>);
+    console.log(this.props.restaurants);
+    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
     return (
       <div>
