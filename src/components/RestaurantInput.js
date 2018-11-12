834b0a471ef948182220bd5c15dacb14fd678a43
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-11-12 13:41:23 -0800

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,9 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    // add missing code
+      debugger
+    this.props.dispatch(addRestaurant(this.state))
+
   }
 
   render() {
@@ -50,5 +52,6 @@ export class RestaurantInput extends Component {
 };
 
 
+
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(addRestaurant)(RestaurantInput)
--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -4,8 +4,8 @@ import { connect } from 'react-redux';
 class Restaurants extends Component {
 
   render() {
-
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.restaurantName}</li>);
+debugger
+    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
     return (
       <div>
