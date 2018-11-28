af1fa77819971c2c88a2cde288958ef59df6c971
Jrodr4544 <maskdbandit@gmail.com>
2018-11-28 17:22:04 +0000

    Done.

--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -4,9 +4,10 @@ export default function manageRestaurants(state = {
   switch (action.type) {
 
     case 'ADD_RESTAURANT':
+
       return {
         ...state,
-        restaurants: [...state.restaurants, action.restaurant]
+        restaurants: [...state.restaurants, action.restaurant]//restaurant]
       }
 
     default:
