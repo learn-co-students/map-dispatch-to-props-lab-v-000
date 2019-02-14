fe545a0eaa24382a008ba9df6ca559f2689c63a3
github username <github email address>
2019-02-14 06:52:37 -0500

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -25,7 +25,6 @@ export class RestaurantInput extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     this.props.addRestaurant(this.state);
-    // add missing code
   }
 
   render() {
