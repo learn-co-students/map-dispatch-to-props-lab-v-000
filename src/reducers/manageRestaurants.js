566015109cd9c3dc755668f79baaec6e7a6fcd03
MiriamPeskowitz <miriam5000@gmail.com>
2019-03-11 13:20:47 -0400

    Done.

--- src/reducers/manageRestaurants.js
+++ src/reducers/manageRestaurants.js
@@ -1,6 +1,7 @@
 export default function manageRestaurants(state = {
   restaurants: []
 }, action) {
+  console.log(action)
   switch (action.type) {
 
     case 'ADD_RESTAURANT':
@@ -10,7 +11,9 @@ export default function manageRestaurants(state = {
       }
 
     default:
+      console.log(state)
       return state;
 
   }
 };
+
