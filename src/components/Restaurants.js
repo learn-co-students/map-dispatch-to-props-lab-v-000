3eb429162a4cd65dd76a036648c8f729bc84677c
terryblue99 <terryblue@comcast.net>
2019-03-28 17:16:53 -0500

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class Restaurants extends Component {
 
   render() {
 
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
+    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>)
 
     return (
       <div>
@@ -13,12 +13,12 @@ class Restaurants extends Component {
           {restaurants}
         </ul>
       </div>
-    );
+    )
   }
-};
+}
 
 const mapStateToProps = state => {
   return { restaurants: state.restaurants }
 }
 
-export default connect(mapStateToProps)(Restaurants);
+export default connect(mapStateToProps)(Restaurants)
