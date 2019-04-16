be2bc3b3f35a36dd6379e839d57697f21a265fcb
github username <github email address>
2019-04-15 18:31:23 -0700

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,7 +23,9 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
+    debugger
     // add missing code
+    this.props.addRestaurant(this.state)
   }
 
   render() {
@@ -51,4 +53,5 @@ export class RestaurantInput extends Component {
 
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+
+export default connect(null, { addRestaurant })(RestaurantInput);
