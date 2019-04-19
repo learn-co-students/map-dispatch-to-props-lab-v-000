0d72d7051972d8a22384b1dcf1424608fe4dc1f4
nichia <nichia@gmail.com>
2019-04-19 18:44:02 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import manageRestaurants from './reducers/manageRestaurants';
 
-const store = createStore(manageRestaurants, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(
+  manageRestaurants,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App store={store}/>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
