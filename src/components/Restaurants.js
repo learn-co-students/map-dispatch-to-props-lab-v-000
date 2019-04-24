6c51db6b71df0087399a41116c83c0a44410e071
julianjohannesen <github email address>
2019-04-24 15:55:07 -0400

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+//import manageRestaurants from '../reducers/manageRestaurants';
 
 class Restaurants extends Component {
 
   render() {
 
+    // We map state to 'restaurants' down below, which means that here we can reference it as this.props.restuarants
+    // which we map over, creating a list item for each restaurant object in the restaurants array.
     let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
     return (
@@ -21,4 +24,5 @@ const mapStateToProps = state => {
   return { restaurants: state.restaurants }
 }
 
+// We don't need to explicitly map dispatch to props
 export default connect(mapStateToProps)(Restaurants);
