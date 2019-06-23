f83c80dab04f1b4e0f0ac8b7ee8f4dd947a49624
roitblatari <roitblatari@gmail.com>
2019-06-23 14:52:10 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import RestaurantInput from './components/RestaurantInput';
 import Restaurants from './components/Restaurants';
 
 export class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <RestaurantInput />
-        <Restaurants />
-      </div>
-    );
-  }
-};
+	render() {
+		return (
+			<div className="App">
+				<RestaurantInput />
+				<Restaurants />
+			</div>
+		);
+	}
+}
 
 export default App;
