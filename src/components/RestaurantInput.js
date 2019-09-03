572bf857cc8195577b959c5a3e26dfcde18e4126
inkanimal <theatomproject@gmail.com>
2019-09-03 10:43:51 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,6 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
+    this.props.addRestaurant(this.state)
     // add missing code
   }
 
