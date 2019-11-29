7ef3da708acd62c91a125ae79b2f0fbf6103a7fd
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-11-29 18:42:28 -0500

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import '../App.js';
+
+
 
 class Restaurants extends Component {
 
   render() {
-
     let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
 
     return (
@@ -17,8 +19,10 @@ class Restaurants extends Component {
   }
 };
 
-const mapStateToProps = state => {
-  return { restaurants: state.restaurants }
-}
+const mapStateToProps = (state) => {
+  return {
+    restaurants: state.restaurants
+  };
+};
 
 export default connect(mapStateToProps)(Restaurants);
