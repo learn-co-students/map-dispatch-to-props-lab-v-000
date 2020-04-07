77e4a580ffd15b4cbc23a587e44b09f3120ac6e0
giancarloz <gzabaleta48@gmail.com>
2020-04-07 15:20:10 -0400

    Done.

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -23,12 +23,7 @@ export class RestaurantInput extends Component {
 
   handleOnSubmit = event => {
     event.preventDefault();
-    const { name, location } = this.state
-    this.props.addRestaurant({name, location})
-    this.setState({
-      name: '',
-      location: ''
-    })
+    this.props.addRestaurant(this.state);
   }
 
   render() {
