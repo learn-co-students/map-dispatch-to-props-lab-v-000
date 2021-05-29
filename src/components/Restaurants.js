280296075a14f4c049366ff3a699057492246b55
emanao <emanao@me.com>
2021-05-29 10:15:24 -0400

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -17,8 +17,5 @@ class Restaurants extends Component {
   }
 };
 
-const mapStateToProps = state => {
-  return { restaurants: state.restaurants }
-}
 
-export default connect(mapStateToProps)(Restaurants);
+export default connect(state=>({restaurants:state.restaurants}))(Restaurants);
